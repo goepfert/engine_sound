e434bba436a47c911116c3f38cf0a1c716827197
Thomas Goepfert <thomas.goepfert@solderlab.de>
2025-03-11 21:37:37 +0100

    Enhance HTML structure and styling; refactor audio handling in JavaScript

--- main.js
+++ main.js
@@ -11,24 +11,22 @@ window.onload = function () {
 
   function createPitchStep(n) {
     return function (e) {
-      if (e.keyCode == 38) {
+      if (e.keyCode === 38) {
         pitch.step = n;
       }
     };
   }
 
-  xhr.open('GET', './engine.wav', true);
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = function (e) {
-    let audioData = this.response;
-
+  function handleAudioData(audioData) {
     window.addEventListener('keydown', createPitchStep(pitchStep));
-    window.addEventListener('keyup', createPitchStep(-1 * pitchStep));
+    window.addEventListener('keyup', createPitchStep(-pitchStep));
 
-    start.addEventListener('click', function (e) {
+    start.addEventListener('click', startEngine);
+    stop.addEventListener('click', stopEngine);
+
+    function startEngine() {
       if (!source) {
         source = audioCtx.createBufferSource();
-
         audioCtx.decodeAudioData(audioData, function (buffer) {
           source.buffer = buffer;
           source.loop = true;
@@ -36,24 +34,30 @@ window.onload = function () {
           source.start();
         });
 
-        intervalId = setInterval(function () {
-          let currPitch = source.playbackRate.value;
-
-          if ((pitch.step < 0 && currPitch > pitch.min) || (pitch.step > 0 && currPitch < pitch.max)) {
-            source.playbackRate.value += pitch.step;
-          }
-        }, 50);
+        intervalId = setInterval(updatePitch, 50);
       }
-    });
+    }
 
-    stop.addEventListener('click', function (e) {
+    function stopEngine() {
       if (source) {
         source.stop();
         source = null;
         clearInterval(intervalId);
       }
-    });
-  };
+    }
 
+    function updatePitch() {
+      const currPitch = source.playbackRate.value;
+      if ((pitch.step < 0 && currPitch > pitch.min) || (pitch.step > 0 && currPitch < pitch.max)) {
+        source.playbackRate.value += pitch.step;
+      }
+    }
+  }
+
+  xhr.open('GET', './engine.wav', true);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function () {
+    handleAudioData(this.response);
+  };
   xhr.send();
 };
